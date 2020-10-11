521e59daf45cc4f61bcbe2b64fce0da6567498da
thanh.nq <momo2941998@gmail.com>
2020-10-11 11:22:54 +0700

    get machine uuid

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
-
+const machineUuid = require("machine-uuid");
 const app = express();
+app.use(express.json());
 
 // parsed property show result reading .env file
 console.log(dotenv.parsed);
 
+app.get('/', (req, res) => {
+    machineUuid().then(uuid => {
+        res.json({uuid: uuid});
+
+    })
+})
+
 const PORT = process.env.PORT || 5000;
-app.listen(5000, () => console.log(`App is running on port ${PORT}`));
+app.listen(PORT, () => console.log(`App is running on port ${PORT}`));
